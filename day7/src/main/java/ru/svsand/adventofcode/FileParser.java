76444607e726204e675682981c578935fbaf5ddd
sand <sve.snd@gmail.com>
2024-12-07 23:37:41 +0300

    repair

---
+++ day7/src/main/java/ru/svsand/adventofcode/FileParser.java
@@ -0,0 +1,48 @@
+package ru.svsand.adventofcode;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author sand <sve.snd@gmail.com>
+ * @since 04.12.2024
+ */
+
+public class FileParser {
+
+    public ArrayList<Equation> read(String fileName) {
+        ArrayList<Equation> equations = new ArrayList<>();
+        List<String> strings = readFile(fileName);
+
+        for (String string: strings) {
+            Equation equation = new Equation();
+            String[] arr = string.split(":");
+            equation.setResult(Long.parseLong(arr[0]));
+
+            Arrays.stream(arr[1].trim().split(" "))
+                    .forEach(
+                            item -> equation.getValues().add(Integer.parseInt(item))
+                    );
+            equations.add(equation);
+        }
+        return equations;
+    }
+
+    private List<String> readFile(String fileName) {
+        List<String> strings;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            strings = br.lines()
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return strings;
+    }
+}
---
+++ day7/src/main/java/ru/svsand/adventofcode/Main.java
@@ -0,0 +1,28 @@
+package ru.svsand.adventofcode;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Advent of code 2024");
+        System.out.println("Day 7: Bridge Repair");
+
+        String fileName = args[0];
+        FileParser parser = new FileParser();
+        ArrayList<Equation> equations = parser.read(fileName);
+
+        long result = 0;
+        for (Equation equation: equations) {
+            if (equation.isValid())
+                result += equation.getResult();
+        }
+        System.out.println("Sum valid equations: " + result);
+
+        result = 0;
+        for (Equation equation: equations) {
+            if (equation.isValidWithConcatenation())
+                result += equation.getResult();
+        }
+        System.out.println("Sum valid equations with concatenation: " + result);
+    }
+}
