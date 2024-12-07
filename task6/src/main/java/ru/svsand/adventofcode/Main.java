e57a0ad3924425ff1f7496786b35fd8ec9081d2d
sand <sve.snd@gmail.com>
2024-12-07 20:30:02 +0300

    day-6: task 1 is done

---
+++ task6/src/main/java/ru/svsand/adventofcode/Main.java
@@ -0,0 +1,17 @@
+package ru.svsand.adventofcode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Advent of code 2024");
+        System.out.println("Day 6: Guard Gallivant");
+
+        String fileName = args[0];
+        FileParser parser = new FileParser();
+        char[][] map = parser.read(fileName);
+
+        Game game = new Game(map);
+        game.start();
+
+        System.out.println("Steps count: " + game.countWay());
+    }
+}
