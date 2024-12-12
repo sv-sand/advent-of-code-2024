7bcf32e9540f70510d7c925766b83150de00fd3d
sand <sve.snd@gmail.com>
2024-12-12 09:01:09 +0300

    day-9: optimization

--- day9/src/main/java/ru/svsand/adventofcode/day9/Main.java
+++ day9/src/main/java/ru/svsand/adventofcode/day9/Main.java
@@ -11,35 +11,11 @@ public class Main {
         FileParser parser = new FileParser();
         ArrayList<Integer> diskMap = parser.read(fileName);
 
-        // Create disk blocks
-        ArrayList<Integer> files = new ArrayList<>();
-        int number = 0;
-        for (int index = 0; index < diskMap.size(); index++, number++) {
-            for (int count = 0; count < diskMap.get(index); count++)
-                files.add(number);
-
-            index++;
-            if (index >= diskMap.size())
-                break;
-            for (int count = 0; count < diskMap.get(index); count++)
-                files.add(-1);
-        }
-
-        // Move blocks
-        for (int index = files.size() - 1; index > 0 ; index--)
-            if (files.get(index) != -1)
-                for (int i = 0; i < index; i++)
-                    if (files.get(i) == -1) {
-                        files.set(i, files.get(index));
-                        files.set(index, -1);
-                    }
-
-        // Add
-        long sum = 0;
-        for (int index = 0; index < files.size(); index++)
-            if (files.get(index) > 0)
-                sum += index * files.get(index);
-
-        System.out.println("Filesystem checksum: " + sum);
+        //while (true) {
+            FileSystem fs = new FileSystem(diskMap);
+            fs.createDiskBlocks();
+            fs.moveBlocks();
+            System.out.println("Filesystem checksum: " + fs.calculateSum());
+        //}
     }
 }
