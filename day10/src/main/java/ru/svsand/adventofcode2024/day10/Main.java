25f4d83372187a8e32e073861890d663d19f1af1
sand <sve.snd@gmail.com>
2025-02-05 10:51:07 +0300

    issue-11: new test for day10

--- day10/src/main/java/ru/svsand/adventofcode2024/day10/Main.java
+++ day10/src/main/java/ru/svsand/adventofcode2024/day10/Main.java
@@ -1,13 +1,15 @@
 package ru.svsand.adventofcode2024.day10;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Advent of code 2024");
         System.out.println("Day 10: Hoof It");
 
         String fileName = args[0];
-        FileParser parser = new FileParser();
-        int[][] topographicMap = parser.read(fileName);
+        List<String> strings = Reader.readFile(fileName);
+        int[][] topographicMap = Parser.read(strings);
 
         TrailsCounterPart1 counter1 = new TrailsCounterPart1(topographicMap);
         counter1.findAllTrails();
---
+++ day10/src/main/java/ru/svsand/adventofcode2024/day10/Reader.java
@@ -0,0 +1,29 @@
+package ru.svsand.adventofcode2024.day10;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author sand <sve.snd@gmail.com>
+ * @since 05.12.2024
+ */
+
+public class Reader {
+
+    public static List<String> readFile(String fileName) {
+        List<String> strings;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            strings = br.lines()
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return strings;
+    }
+
+}
