034416a04bbe3da4176ea586f0ab093d53e4f9ab
sand <sve.snd@gmail.com>
2025-02-11 13:51:40 +0300

    issue-15: add Sonar code check

--- day3/src/main/java/ru/svsand/adventofcode2024/day3/Main.java
+++ day3/src/main/java/ru/svsand/adventofcode2024/day3/Main.java
@@ -1,17 +1,12 @@
 package ru.svsand.adventofcode2024.day3;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Welcome to Advent of code 2024");
         System.out.println("Day 3: Mull It Over");
 
         String fileName = args[0];
-        String text = new Scanner(new File(fileName)).useDelimiter("\\Z").next();
-
+        String text = Reader.readFile(fileName).get(0);
         InstructionMultiplier multiplier = new InstructionMultiplier(text);
 
         System.out.println("Result of instructions: " + multiplier.multiply());
