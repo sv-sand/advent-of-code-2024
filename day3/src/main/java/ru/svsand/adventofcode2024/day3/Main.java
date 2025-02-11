c2f16934e571473f7a55443ebc26fb55e7d78944
sand <sve.snd@gmail.com>
2025-02-11 22:21:52 +0300

    issue-20: Scanner deleted

--- day3/src/main/java/ru/svsand/adventofcode2024/day3/Main.java
+++ day3/src/main/java/ru/svsand/adventofcode2024/day3/Main.java
@@ -1,20 +1,17 @@
 package ru.svsand.adventofcode2024.day3;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
         
         logger.info("Welcome to Advent of code 2024");
         logger.info("Day 3: Mull It Over");
 
         String fileName = args[0];
-        String text = new Scanner(new File(fileName)).useDelimiter("\\Z").next();
+        String text = Reader.readFile(fileName).get(0);
 
         InstructionMultiplier multiplier = new InstructionMultiplier(text);
 
